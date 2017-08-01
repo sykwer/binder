23c8511dabdf6adc2bf8a15777426e49610724ed
sykwer <sykwer@gmail.com>
2017-08-02 04:53:36 +0900

    Follow instruction of devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # TODO: Configure default_url_options for production env
+  # config.action_mailer.default_url_options = { host: 'actual host address', port: 22 }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
