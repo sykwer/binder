b182412201591e685573686b6ce654729baa9260
sykwer <sykwer@gmail.com>
2017-08-02 16:54:02 +0900

    Define route named new_user_session

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#top'
-  get 'login', to: 'sessions#new'
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
+    get 'login', to: 'sessions#new', as: :new_user_session
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 end
