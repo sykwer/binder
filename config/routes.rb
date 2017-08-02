626581047b7cfd553087615efce9583cb809a177
sykwer <sykwer@gmail.com>
2017-08-02 12:14:07 +0900

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#top'
+  get 'login', to: 'sessions#new'
   devise_for :users
 end
