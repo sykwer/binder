67260cbc81a8cdc0fa9a05d61e0fc917109ec1aa
sykwer <sykwer@gmail.com>
2017-08-04 03:55:22 +0900

    Revert

--- app/assets/javascripts/pages/top.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  var mainWrapperHeight = parseInt($('#article-main-wrapper').css('height'));
-  $('#article-book-review').css('height', mainWrapperHeight * 0.9);
-  $('#article-book-review-below').css('height', mainWrapperHeight * 0.1);
-});
