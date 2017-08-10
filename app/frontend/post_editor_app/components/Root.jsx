34215e3a4ffc4b176160861c145d5961b5e7e43b
sykwer <sykwer@gmail.com>
2017-08-11 08:54:10 +0900

    Pass store to Root component

--- app/frontend/post_editor_app/components/Root.jsx
+++ app/frontend/post_editor_app/components/Root.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+import rootReducer from "../store/reducers"
+
+const store = createStore(rootReducer)
 
 const Root = () => (
-  <p>hello syker from root component</p>
+  <Provider store={store}>
+    <p>hello sykwer from Root component</p>
+  </Provider>
 )
 
 export default Root
---
+++ app/frontend/post_editor_app/store/reducers.js
@@ -0,0 +1,30 @@
+import { combineReducers } from "redux"
+
+const searchInput = (state, action) => {
+}
+
+const bookList = (state, action) => {
+}
+
+const selectedBookAsin = (state, action) => {
+}
+
+const postContent = (state, action) => {
+}
+
+const contentSaveState = (state, action) => {
+}
+
+const bookSearchState = (state, action) => {
+}
+
+const rootReducer = combineReducers({
+  searchInput,
+  bookList,
+  selectedBookAsin,
+  postContent,
+  contentSaveState,
+  bookSearchState,
+})
+
+export default rootReducer
