55d75b251a9a60108912c0fecc24e54d3baa738a
sykwer <sykwer@gmail.com>
2017-08-16 16:12:29 +0900

    Add BookInfoInput components

--- app/frontend/post_editor_app/components/Root.jsx
+++ app/frontend/post_editor_app/components/Root.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 
 import PostContentContainer from "./PostContentContainer"
+import BookInfoInputContainer from "./BookInfoInputContainer"
 
 const Root = () => (
   <div className="post-editor-component clearfix">
+    <div className="book-selector-component">
+      <BookInfoInputContainer />
+    </div>
     <div className="main-wrapper clearfix">
       <div className="main-right">
         <PostContentContainer />
