7faf58b2a1091df6da27f57ad640eb5e551ae89c
sykwer <sykwer@gmail.com>
2017-09-12 05:10:14 +0900

    Add empty userProfileApp

---
+++ app/frontend/packs/user_profile_app.jsx
@@ -0,0 +1,9 @@
+import { render } from "react-dom"
+import React from "react"
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <p>hello</p>,
+    document.getElementById("user-profile-app"),
+  )
+})
