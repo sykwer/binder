d205b5aafa3a7771435796ae81a2db35c419bbd5
sykwer <sykwer@gmail.com>
2017-09-21 11:26:48 +0900

    Fanout when publish

--- app/controllers/posts/contents_controller.rb
+++ app/controllers/posts/contents_controller.rb
@@ -3,6 +3,10 @@ class Posts::ContentsController < ApplicationController
 
   def update
     @post.publish_or_update_content!
+
+    follower_ids = Follow.where(destination_id: current_user.id).pluck(:source_id)
+    PostFanoutService.new(target_user_ids: follower_ids, post_uuid: @post.uuid).fanout
+
     redirect_to root_path
   end
 
