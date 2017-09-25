774ae8ba4b5637c079ff93a727f3a6b35e1711fd
sykwer <sykwer@gmail.com>
2017-09-25 21:57:38 +0900

    Add counter cache

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
-  belongs_to :post, foreign_key: :post_uuid
+  belongs_to :post, foreign_key: :post_uuid, counter_cache: :bookmarked_count
 end
