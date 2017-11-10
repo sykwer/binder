78ad367eb3ecc669763633eba92d19f4faaad3d9
sykwer <sykwer@gmail.com>
2017-11-10 20:48:44 +0900

    Change basic auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic_auth! if Rails.env.production?
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"], password: ENV["BASIC_AUTH_PASSWORD"] if Rails.env.production?
 
   # We are not using :database_authenticatable, we have to define the helper method new_session_path(scope)
   # so it can correctly redirect in case of failure.
   def new_session_path(scope)
     new_user_session_path
-  end
-
-  private
-
-  def basic_auth!
-    authenticate_or_request_with_http_basic do |username, password|
-      Devise.secure_compare(username, ENV["BASIC_AUTH_USERNAME"]) &&
-        Devise.secure_compare(password, ENV["BASIC_AUTH_PASSWORD"])
-    end
   end
 end
