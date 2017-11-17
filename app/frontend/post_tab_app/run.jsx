ac5b43c5de1cb72f5411321ed2eddbf1049068de
sykwer <sykwer@gmail.com>
2017-11-17 19:32:18 +0900

    Init reducer

--- app/frontend/post_tab_app/run.jsx
+++ app/frontend/post_tab_app/run.jsx
@@ -1,12 +1,28 @@
 import React from "react"
 import { render } from "react-dom"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+import rootReducer from "./store/reducers"
 
 const run = () => {
   document.addEventListener("DOMContentLoaded", () => {
     const node = document.getElementById("post-tab-app")
+    const data = JSON.parse(node.getAttribute("data"))
+
+    const initialState = {
+      userId: data.userId,
+    }
+
+    const store = createStore(
+      rootReducer,
+      initialState,
+    )
 
     render(
-      <p>Boot post tab app from here</p>,
+      <Provider store={store}>
+        <p>Boot post tab app from here</p>
+      </Provider>,
       node,
     )
   })
---
+++ app/frontend/post_tab_app/store/reducers.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux"
+
+// eslint-disable-next-line
+const userId = (state = null, action) => {
+  return state
+}
+
+const rootReducer = combineReducers({
+  userId,
+})
+
+export default rootReducer
