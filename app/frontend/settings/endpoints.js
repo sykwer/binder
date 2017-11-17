a72eda2201971c2a581160437a8c87175b6d5ba9
sykwer <sykwer@gmail.com>
2017-11-17 19:23:23 +0900

    Frontend settings for stg

--- app/frontend/settings/endpoints.js
+++ app/frontend/settings/endpoints.js
@@ -1,9 +1,23 @@
 /* eslint-disable global-require, import/no-dynamic-require */
 
+let fileEnv
+switch (process.env.NODE_ENV) {
+  case "production":
+    fileEnv = "prod"
+    break
+  case "staging":
+    fileEnv = "stg"
+    break
+  case "development":
+    fileEnv = "dev"
+    break
+  default:
+}
+
 export const {
   binderApiEndpoint,
   binderRootUrl,
-} = require(`./endpoints.${process.env.NODE_ENV === "production" ? "prod" : "dev"}.js`)
+} = require(`./endpoints.${fileEnv}.js`)
 
 export const {
   googleBookApiEndpoint,
