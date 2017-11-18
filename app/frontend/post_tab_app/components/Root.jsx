fd2e28396fb0ab0f61c26d964c98d7a2adf544e1
sykwer <sykwer@gmail.com>
2017-11-18 18:37:34 +0900

    Add BottomFixedWindow

--- app/frontend/post_tab_app/components/Root.jsx
+++ app/frontend/post_tab_app/components/Root.jsx
@@ -1,13 +1,33 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 
 import PostsList from "./PostsList"
+import BottomFixedWindow from "./BottomFixedWindow"
 import BottomObserver from "./BottomObserver"
 
-const Root = () => (
+const cpnt = ({ isBottomWindowDisplayed }) => (
   <div>
     <PostsList />
+    {
+      isBottomWindowDisplayed && (
+        <BottomFixedWindow />
+      )
+    }
     <BottomObserver />
   </div>
 )
+
+cpnt.propTypes = {
+  isBottomWindowDisplayed: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+  isBottomWindowDisplayed: state.selectedPostUuids.length > 0,
+})
+
+const Root = connect(
+  mapStateToProps,
+)(cpnt)
 
 export default Root
