35cfc7b03889ca0159daee3906591050c3a5bce7
sykwer <sykwer@gmail.com>
2017-11-21 13:28:28 +0900

    OnClickOutside

--- app/frontend/action_bar_config/components/Root.jsx
+++ app/frontend/action_bar_config/components/Root.jsx
@@ -1,115 +1,120 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
+import onClickOutside from "react-onclickoutside"
 
 import { closeWindow, openWindow } from "../store/actions"
 
-const cpnt = ({
-  handleCloseWindow,
-  handleOpenWindow,
-  isWindowOpen,
-  name,
-  username,
-  destroySessionPath,
-}) => (
-  <div
-    className="config-icon-wrapper cancel-focus-outline"
-    role="button"
-    tabIndex="0"
-    onClick={(e) => {
-      e.stopPropagation()
-      handleCloseWindow()
-    }}
-  >
-    <i
-      className="fa fa-cog config-icon cancel-focus-outline"
-      aria-hidden="true"
-      role="button"
-      tabIndex="0"
-      onClick={(e) => {
-        e.stopPropagation()
-        if (isWindowOpen) {
+class cpnt extends Component {
+  handleClickOutside() {
+    this.props.handleCloseWindow()
+  }
+
+  render() {
+    const { handleCloseWindow, handleOpenWindow } = this.props
+    const { isWindowOpen, name, username, destroySessionPath } = this.props
+
+    return (
+      <div
+        className="config-icon-wrapper cancel-focus-outline"
+        role="button"
+        tabIndex="0"
+        onClick={(e) => {
+          e.stopPropagation()
           handleCloseWindow()
-        } else {
-          handleOpenWindow()
-        }
-      }}
-    />
-    {
-      isWindowOpen && (
-        <div>
-          <div
-            className="config-window-before cancel-focus-outline"
-            role="button"
-            tabIndex="0"
-            onClick={(e) => { e.stopPropagation()() }}
-          />
-          <div
-            className="config-window cancel-focus-outline"
-            role="button"
-            tabIndex="0"
-            onClick={(e) => { e.stopPropagation() }}
-          >
-            <div className="head-box">
-              <a href={`@${username}`}>
-                <p className="login-name">{name}</p>
-              </a>
-              <a href={`@${username}`}>
-                <p className="login-name-comment">マイページを表示</p>
-              </a>
-              <i
-                className="fa fa-times close-window-button cancel-focus-outline"
-                aria-hidden="true"
+        }}
+      >
+        <i
+          className="fa fa-cog config-icon cancel-focus-outline"
+          aria-hidden="true"
+          role="button"
+          tabIndex="0"
+          onClick={(e) => {
+            e.stopPropagation()
+            if (isWindowOpen) {
+              handleCloseWindow()
+            } else {
+              handleOpenWindow()
+            }
+          }}
+        />
+        {
+          isWindowOpen && (
+            <div>
+              <div
+                className="config-window-before cancel-focus-outline"
                 role="button"
                 tabIndex="0"
-                onClick={(e) => {
-                  e.stopPropagation()
-                  handleCloseWindow()
-                }}
+                onClick={(e) => { e.stopPropagation()() }}
+              />
+              <div
+                className="config-window cancel-focus-outline"
+                role="button"
+                tabIndex="0"
+                onClick={(e) => { e.stopPropagation() }}
+              >
+                <div className="head-box">
+                  <a href={`@${username}`}>
+                    <p className="login-name">{name}</p>
+                  </a>
+                  <a href={`@${username}`}>
+                    <p className="login-name-comment">マイページを表示</p>
+                  </a>
+                  <i
+                    className="fa fa-times close-window-button cancel-focus-outline"
+                    aria-hidden="true"
+                    role="button"
+                    tabIndex="0"
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      handleCloseWindow()
+                    }}
+                  />
+                </div>
+                <ul className="config-list">
+                  <li className="config-list-item">
+                    アカウント設定
+                  </li>
+                  <li className="config-list-item">
+                    お問い合わせ
+                  </li>
+                  <a
+                    href={destroySessionPath}
+                    data-method="delete"
+                  >
+                    <li className="config-list-item cancel-focus-outline">
+                      ログアウト
+                    </li>
+                  </a>
+                </ul>
+                <ul className="config-list">
+                  <li className="config-list-item bold-item">
+                    Binder目安箱
+                  </li>
+                </ul>
+                <div
+                  className="foot-box calcel-focus-outline"
+                  role="button"
+                  tabIndex="0"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    handleCloseWindow()
+                  }}
+                />
+              </div>
+              <div
+                className="config-window-after cancel-focus-outline"
+                role="button"
+                tabIndex="0"
+                onClick={(e) => { e.stopPropagation() }}
               />
             </div>
-            <ul className="config-list">
-              <li className="config-list-item">
-                アカウント設定
-              </li>
-              <li className="config-list-item">
-                お問い合わせ
-              </li>
-              <a
-                href={destroySessionPath}
-                data-method="delete"
-              >
-                <li className="config-list-item cancel-focus-outline">
-                  ログアウト
-                </li>
-              </a>
-            </ul>
-            <ul className="config-list">
-              <li className="config-list-item bold-item">
-                Binder目安箱
-              </li>
-            </ul>
-            <div
-              className="foot-box calcel-focus-outline"
-              role="button"
-              tabIndex="0"
-              onClick={(e) => {
-                e.preventDefault()
-                handleCloseWindow()
-              }}
-            />
-          </div>
-          <div
-            className="config-window-after cancel-focus-outline"
-            role="button"
-            tabIndex="0"
-            onClick={(e) => { e.stopPropagation() }}
-          />
-        </div>
-      )
-    }
-  </div>
-)
+          )
+        }
+      </div>
+    )
+  }
+}
 
 cpnt.propTypes = {
   isWindowOpen: PropTypes.bool.isRequired,
@@ -119,6 +124,8 @@ cpnt.propTypes = {
   handleCloseWindow: PropTypes.func.isRequired,
   handleOpenWindow: PropTypes.func.isRequired,
 }
+
+const wrappedCpnt = onClickOutside(cpnt)
 
 const mapStateToProps = state => ({
   isWindowOpen: state.isWindowOpen,
@@ -139,6 +146,6 @@ const mapDispatchToProps = dispatch => ({
 const Root = connect(
   mapStateToProps,
   mapDispatchToProps,
-)(cpnt)
+)(wrappedCpnt)
 
 export default Root
