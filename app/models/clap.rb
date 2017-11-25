2359680c39d8142b24c0ed4b32c3e6beeb55ea39
sykwer <sykwer@gmail.com>
2017-11-25 14:17:14 +0900

    Prohibit self clap in model level

--- app/models/clap.rb
+++ app/models/clap.rb
@@ -2,10 +2,19 @@ class Clap < ApplicationRecord
   belongs_to :user
   belongs_to :post, foreign_key: :post_uuid
 
+  validate :user_must_not_self_clap
+
   def countup!
     ActiveRecord::Base.transaction do
       increment!(:count)
       post.increment!(:claps_count)
     end
+  end
+
+  private
+
+  def user_must_not_self_clap
+    return if user != post.user
+    errors.add(:clap, "must not be from post owner")
   end
 end
