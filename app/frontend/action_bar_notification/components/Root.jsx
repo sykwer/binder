69d0d248f21ef09fc7c4b9d961c27024bba433b0
sykwer <sykwer@gmail.com>
2017-12-06 23:07:09 +0900

    Add empty notification window

--- app/frontend/action_bar_notification/components/Root.jsx
+++ app/frontend/action_bar_notification/components/Root.jsx
@@ -1,12 +1,83 @@
-import React from "react"
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import onClickOutside from "react-onclickoutside"
 
-const Root = () => (
-  <div className="notification-icon-wrapper">
-    <i
-      className="notification-icon fa fa-bell-o"
-      aria-hidden="true"
-    />
-  </div>
-)
+import { openWindow, closeWindow } from "../store/actions"
+
+class cpnt extends Component {
+  handleClickOutside() {
+    this.props.handleCloseWindow()
+  }
+
+  render() {
+    const { handleOpenWindow } = this.props
+    const { isWindowOpen } = this.props
+
+    return (
+      <div className="notification-icon-wrapper">
+        <i
+          className="fa fa-bell-o notification-icon cancel-focus-outline"
+          aria-hidden="true"
+          tagIndex="0"
+          onClick={(e) => {
+            e.stopPropagation()
+            handleOpenWindow()
+          }}
+        />
+        {
+          isWindowOpen && (
+            <div>
+              <div
+                className="notification-window-before cancel-focus-outline"
+                role="button"
+                tabIndex="0"
+                onClick={(e) => { e.stopPropagation() }}
+              />
+              <div
+                className="notification-window cancel-focus-outline"
+                role="button"
+                tabIndex="0"
+                onClick={(e) => { e.stopPropagation() }}
+              />
+              <div
+                className="notification-window-after cancel-focus-outline"
+                role="button"
+                tabIndex="0"
+                onClick={(e) => { e.stopPropagation() }}
+              />
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+cpnt.propTypes = {
+  isWindowOpen: PropTypes.bool.isRequired,
+  handleCloseWindow: PropTypes.func.isRequired,
+  handleOpenWindow: PropTypes.func.isRequired,
+}
+
+const wrappedCpnt = onClickOutside(cpnt)
+
+const mapStateToProps = state => ({
+  isWindowOpen: state.isWindowOpen,
+})
+
+const mapDispatchToProps = dispatch => ({
+  handleCloseWindow: () => {
+    dispatch(closeWindow())
+  },
+  handleOpenWindow: () => {
+    dispatch(openWindow())
+  },
+})
+
+const Root = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(wrappedCpnt)
 
 export default Root
