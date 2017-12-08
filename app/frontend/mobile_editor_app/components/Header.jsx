ee7e75df2ada8f463b49b70237dc7b448b6e4163
sykwer <sykwer@gmail.com>
2017-12-09 04:32:33 +0900

    Implement Header

--- app/frontend/mobile_editor_app/components/Header.jsx
+++ app/frontend/mobile_editor_app/components/Header.jsx
@@ -1,7 +1,92 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 
-const Header = () => (
-  <div />
+const cpnt = ({
+  isSaving,
+  publicationStatus,
+}) => (
+  <div className="mobile-editor-header">
+    <a
+      className="close-button cancel-focus-outline"
+      role="button"
+      tabIndex="0"
+      onClick={(e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        window.history.back()
+      }}
+    >
+      Close
+    </a>
+    <p className="save-status">
+      {isSaving ? "Saving..." : "Saved"}
+    </p>
+    <p className="publish-status">
+      {publicationStatus}
+    </p>
+    <button className="next-button">
+      Next
+    </button>
+  </div>
 )
+
+const stateToIsSaving = (state) => {
+  let isContentSaving
+  switch (state.contentSaveState) {
+    case "INITIAL":
+    case "IS_SAVED":
+      isContentSaving = false
+      break
+    case "IS_NOT_SAVED":
+    case "IS_SAVING":
+      isContentSaving = true
+      break
+    default:
+      throw new Error("Invalid contentSaveState")
+  }
+
+  let isTitleSaving
+  switch (state.contentSaveState) {
+    case "INITIAL":
+    case "IS_SAVED":
+      isTitleSaving = false
+      break
+    case "IS_NOT_SAVED":
+    case "IS_SAVING":
+      isTitleSaving = true
+      break
+    default:
+      throw new Error("Invalid titleSaveState")
+  }
+
+  return isContentSaving || isTitleSaving
+}
+
+const stateToPublicationStatus = (state) => {
+  if (!state.isPublished) {
+    return "Draft"
+  }
+
+  if (state.isChangesUnpublished) {
+    return "Changes unpublished"
+  }
+
+  return "Open to public"
+}
+
+cpnt.propTypes = {
+  isSaving: PropTypes.bool.isRequired,
+  publicationStatus: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+  isSaving: stateToIsSaving(state),
+  publicationStatus: stateToPublicationStatus(state),
+})
+
+const Header = connect(
+  mapStateToProps,
+)(cpnt)
 
 export default Header
--- app/frontend/mobile_editor_app/run.jsx
+++ app/frontend/mobile_editor_app/run.jsx
@@ -11,12 +11,34 @@ import Root from "./components/Root"
 const run = () => {
   document.addEventListener("DOMContentLoaded", () => {
     const node = document.getElementById("mobile-editor-app")
+    const data = JSON.parse(node.getAttribute("data"))
+
+    const initialState = {
+      uuid: data.uuid,
+      selectedBookAsin: data.bookAsin,
+      selectedBookTitle: data.bookTitle,
+      selectedBookAuthor: data.bookAuthor,
+      selectedBookPublisher: data.bookPublisher,
+      selectedBookImage: data.image,
+      postTitle: data.title,
+      postContent: data.content,
+      date: data.date,
+      isPublished: data.isPublished,
+      isChangesUnpublished: data.isChangesUnpublished,
+      user: {
+        name: data.user.name,
+        username: data.user.username,
+        image: data.user.image,
+      },
+      selectedTags: data.tags,
+      logoImage: data.logoImage,
+    }
 
     const sagaMiddleware = createSagaMiddleware()
 
     const store = createStore(
       rootReducer,
-      {},
+      initialState,
       applyMiddleware(sagaMiddleware),
     )
 
