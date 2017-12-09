36ccbf2303e5397dc8af28a9d1b79502610abd3f
sykwer <sykwer@gmail.com>
2017-12-09 19:27:34 +0900

    Speed up

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,28 @@
-const environment = require('./environment')
+const webpack = require('webpack')
+const environment = require('./environment').toWebpackConfig()
 
-module.exports = environment.toWebpackConfig()
+// https://qiita.com/mizchi/items/a329ab567ba2d3cb7d60
+environment.plugins = environment.plugins.map((plug) => {
+  if (plug instanceof webpack.optimize.UglifyJsPlugin) {
+    return new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      parallel: true,
+      mangle: false,
+      uglifyOptions: {
+        mangle: false
+      },
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  }
+
+  return plug
+})
+
+environment.devtool = 'eval'
+
+module.exports = environment
